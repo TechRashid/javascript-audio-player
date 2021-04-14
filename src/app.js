3301a1b46dd81dca051fda4742196979ce4b9ac1
TechRashid <71259639+TechRashid@users.noreply.github.com>
2021-04-14 12:21:35 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import AudioPlayer from './AudioPlayer';
 
 const player = new AudioPlayer('.audioPlayer', [
-    { name: 'OvO', url: 'songs/Ovo.mp3' },
+    { name: 'Laszlo - Supernova', url: 'songs/Ovo.mp3' },
     { name: 'Regrets', url: 'songs/Regrets.mp3' },
     { name: 'Night Driver', url: 'songs/Night_Driver.mp3' },
     { name: 'Opened Eyes Woke', url: 'songs/Opened_Eyes_Woke.mp3' },
